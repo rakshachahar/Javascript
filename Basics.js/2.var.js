f1fefb1c1af2b8baa1266794167600c95fa45290
Raksha Chahar <rakshachahar336@gmail.com>
2025-10-10 08:22:16 +0000

    JS variables

---
+++ Basics.js/2.var.js
@@ -0,0 +1,30 @@
+
+// var let const
+
+const name = "Raksha"
+{const name= "yash"
+ console.log(name);}
+// name= "yash" [No re-declaring]
+console.log(name);
+
+
+let age = 16
+{let age = 18 //re-assginment 
+// let age = 20 [No re-declaring]
+console.log(age);}
+console.log(age);
+
+
+var salary = 20000
+salary = 40000 
+var salary= 60000
+console.log(salary);
+
+
+let name1 = "Priya"
+let age1 = 20
+let course = "B.Ph"
+location = "Jhajjar"
+let study; //undefined
+console.table([name1, age1,course, location , study])
+
