713d27ed21ec4ac2d64e5249bf6044c56f1b3365
Raksha Chahar <rakshachahar336@gmail.com>
2025-10-25 15:24:00 +0000

    JS Math Object Methods

---
+++ Basics.js/13.math.js
@@ -0,0 +1,24 @@
+// Math object Methods
+
+console.log(Math.abs(-12)); // 12
+console.log(Math.ceil(4.1)); // 5 (return to upper value)
+console.log(Math.floor(4.7)); // 4 (return to lower value)
+console.log(Math.round(4.6)); // 5 (round off to nearest integer)
+console.log(Math.trunc(46.3838384)); // 46 (remove decimal part)
+console.log(Math.max(1, 5, 3, 9, 2)); // 9
+console.log(Math.min(1, 5, 3, 9, 2)); // 1
+console.log(Math.pow(2, 3)); // 8 (2^3)
+console.log(Math.sqrt(16)); // 4
+console.log(Math.cbrt(27)); // 3
+console.log(Math.random()); // random number between 0 and 1    
+console.log(Math.floor(Math.random() * 100) + 1); // random number between 1 and 100
+
+// Constants
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.E); // 2.718281828459045
+console.log(Math.LN2); // 0.6931471805599453 (log base 2)
+
+console.log(Math.log(1));
+console.log(Math.log10(100)); // 10^2=10
+
+// It also supports trignometric functions 
